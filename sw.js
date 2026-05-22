50423ba89191cf7a7437032711cc74d1f6a2e2f4
b22pchg-arch <b22pchg@gmail.com>
2026-05-22 16:37:33 +0700

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-// Nhật ký cập nhật AirGap SCADA: Tích hợp nút check thủ công - Ngày 19/05/2026 QR moi
+// Nhật ký cập nhật AirGap SCADA: Tích hợp nút check thủ công - Ngày 19/05/2026
 const CACHE_NAME = 'AirGapSCADA-Static-Cache'; 
 const ASSETS = [
   '',
